fe12f9c02f3b6f5c53c26faec022ccec7b6f7604
Sudipta <sudipta.anupam.datta@dohatec.com.bd>
2023-09-14 14:13:43 +0600

    just pushing, busy right now

---
+++ 03-frontend/angular-ecommerce/src/app/components/cart-details/cart-details.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { CartItem } from 'src/app/common/cart-item';
+import { CartService } from 'src/app/services/cart.service';
+
+@Component({
+  selector: 'app-cart-details',
+  templateUrl: './cart-details.component.html',
+  styleUrls: ['./cart-details.component.css']
+})
+export class CartDetailsComponent implements OnInit {
+
+  constructor(private cartService : CartService){}
+
+  cartItem : CartItem[] = [];
+  totalPrice : number = 0.00;
+  totalQuantity : number = 0;
+
+  ngOnInit(): void {
+    this.listCartDetails();
+  }
+
+  listCartDetails(){
+
+    //get a handle to the cart items
+    this.cartItem = this.cartService.cartItem;
+
+    //subscribe to the cart total price
+    this.cartService.totalPrice.subscribe(
+      data => {
+        this.totalPrice = data;
+      }
+    );
+
+    //subscribe to the cart total quantity
+    this.cartService.totalQuantity.subscribe(
+      data => {
+        this.totalQuantity = data;
+      }
+    );
+
+    //compute cart total price and quantity
+    this.cartService.computeCartTotals();
+
+
+  }
+
+}
